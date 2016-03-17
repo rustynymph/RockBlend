87da4ef2a8800075d4315dbcdde45f3dab427787
shadowfax6894 <anniekelly6894@gmail.com>
2016-03-17 17:45:26 -0600

    stuff

--- blender.py
+++ blender.py
@@ -1,18 +1,35 @@
 from Tkinter import *
 import Tkinter as ttk
 
+def selectKey(*args):
+	keylabel.pack_forget()
+	keyoption.pack_forget()	
+	selectMode.pack_forget()
+	modeOption.pack_forget()
+	modeButton.pack_forget()	
+	goButton.pack_forget()
+	backButton.pack_forget()
+	cMajorLabel.pack_forget()
+
+	keyChoice = str(key.get())
+	if keyChoice == "C Major":
+		cMajorLabel.pack()
+
 
 def changeMode(*args):
 	mode = str(var.get())
 	keylabel.pack_forget()
 	keyoption.pack_forget()	
+	goButton.pack_forget()
 	selectMode.pack_forget()
 	modeOption.pack_forget()
 	modeButton.pack_forget()
+	cMajorLabel.pack_forget()
 
 	if mode == "Practice":
 		keylabel.pack()
 		keyoption.pack()
+		goButton.pack()
 
 	backButton.pack()
 	center(root) #must be at bottom
@@ -23,10 +40,12 @@ def goBack(*args):
 	selectMode.pack_forget()
 	modeOption.pack_forget()
 	modeButton.pack_forget()	
+	goButton.pack_forget()
 	selectMode.pack()
 	modeOption.pack()
 	modeButton.pack()	
 	backButton.pack_forget()
+	cMajorLabel.pack_forget()
 
 def center(win):
     win.update_idletasks()
@@ -68,11 +87,21 @@ key = StringVar(mainframe)
 key.set("C Major")
 keyoption = OptionMenu(mainframe, key, "C Major", "E Major")
 keyoption.configure(font=("Century Gothic", 20))
+goButton = Button(mainframe, text="GO", command=selectKey, fg="#a1dbcd", bg="#383a39", font=("Century Gothic", 20))
 
 backButton = Button(mainframe, text="GO BACK", command=goBack, fg="#a1dbcd", bg="#383a39", font=("Century Gothic", 20))
+
+cMajor = "The C major scale consists of the \
+pitches C, D, E, F, G, A, and B. Its key \
+signature has no flats and no sharps. Its \
+relative minor is A minor and its parallel \
+minor is C minor."
+cMajorLabel = ttk.Label(mainframe, text=cMajor, fg="#FFFFFF", bg="#000000", font=("Century Gothic", 10))
 
 center(root) #must be at bottom
 
 root.bind('<Return>', changeMode)
 
 root.mainloop()
+
+
---
+++ midi.py
@@ -0,0 +1,38 @@
+import sys, pygame, pygame.midi, serial, time
+
+# set up pygame
+pygame.init()
+pygame.midi.init()
+
+ser = serial.Serial('/dev/ttyACM5', 9600)
+
+lastNotePlayed = 0;
+timeNotePlayed = 0;
+timeDiff = 1;
+
+# list all midi devices
+for x in range( 0, pygame.midi.get_count() ):
+	print pygame.midi.get_device_info(x)
+
+# open a specific midi device
+inp = pygame.midi.Input(3)
+
+# run the event loop
+while True:
+	if (timeDiff < 1):
+		ser.write("1\n")
+	else:
+		ser.write("0\n")
+	if inp.poll():
+		# no way to find number of messages in queue
+		# so we just specify a high max value
+		msg = inp.read(1000)
+		if msg[0][0][2] == 100:
+			lastNotePlayed = timeNotePlayed
+			timeNotePlayed = time.time()
+			timeDiff = timeNotePlayed - lastNotePlayed
+			print timeDiff
+			
+# wait 10ms - this is arbitrary, but wait(0) still resulted
+# in 100% cpu utilization
+pygame.time.wait(10)
