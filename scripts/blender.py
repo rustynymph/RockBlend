444693ae99d8b26b99690567fc2734f59c3a8864
shadowfax6894 <anniekelly6894@gmail.com>
2016-04-13 19:57:01 -0600

    blah

--- scripts/blender.py
+++ scripts/blender.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 import Tkinter as ttk
+import os, sys, pygame, pygame.midi, serial, time, threading
 winWidth = 1500
 winHeight = 600
 mainImageWidth = 447
@@ -115,5 +116,133 @@ practiceText = None
 center(root) #must be at bottom
 root.bind('<Return>', practiceMode)
 root.mainloop()
+
+
+
+
+
+
+
+# =========================================== #
+			#Midi & blender stuff
+# =========================================== #
+
+ser = None
+notesPlayed = 0;
+correctNotes = 0;
+incorrectNotes = 0;
+notes = []
+
+def freestyleBlenderState():
+	global notesPlayed
+	threading.Timer(2.0, freestyleBlenderState).start()
+	avgCount = notesPlayed / 4
+	if avgCount >= 1:
+		#print "on"
+		serialWrite('1')
+	else:
+		#print "off"
+		serialWrite('0')
+	notesPlayed = 0
+
+def practiceBlenderState():
+	global correctNotes
+	global incorrectNotes
+	threading.Timer(2.5, practiceBlenderState).start()
+	totalNotesPlayed = correctNotes + incorrectNotes
+	if (totalNotesPlayed > 0):
+		amountIncorrect = float(incorrectNotes) / float(totalNotesPlayed)
+		if amountIncorrect < 0.2:
+			#print "on"
+			serialWrite('1')
+		else:
+			#print "off"
+			serialWrite('0')
+	else:
+		#print "off"
+		serialWrite('0')
+	correctNotes = 0	
+	incorrectNotes = 0	
+
+def serialWrite(msg):
+	global ser
+	ser.write(msg)
+
+def freestyleListener(inp):
+	global notesPlayed
+	while True:
+		if inp.poll():
+			# no way to find number of messages in queue
+			# so we just specify a high max value
+			msg = inp.read(1000)
+			if msg[0][0][2] == 100: #this is a note pressed event
+				notesPlayed += 1
+
+def practiceListener(inp):
+	global notes
+	global correctNotes
+	global incorrectNotes
+	while True:
+		if inp.poll():
+			# no way to find number of messages in queue
+			# so we just specify a high max value
+			msg = inp.read(1000)
+			if msg[0][0][2] == 100: #this is a note pressed event
+				notePressed = int(msg[0][0][1])
+				if notePressed in notes:
+					correctNotes += 1
+				else:
+					incorrectNotes += 1	
+
+def initMidiScales(key, mode):
+	global notes
+	pitches = {'c':60, 'c#':61, 'd':62, 'd#':63, 'e':64, 'f':65, 'f#':66, 'g':67, 'g#':68, 'a':69, 'a#':70, 'b':71}
+	middleNote = pitches[key]
+	octaves = []
+	# major = W-W-H-W-W-W-H
+	# minor = W-H-W-W-H-W-W
+	if mode == 'major':
+		notes += [middleNote, middleNote+2, middleNote+4, middleNote+5, middleNote+7, middleNote+9, middleNote+11]
+	else:
+		notes += [middleNote, middleNote+2, middleNote+3, middleNote+5, middleNote+7, middleNote+8, middleNote+10]
+		
+	for note in notes:
+		octaves += [note-12, note-24, note+12, note+24]
+
+	notes += octaves
+
+
+def initSerialConn():
+	global ser
+	serialPort = '/dev/ttyACM0'
+	ser = serial.Serial(serialPort, 9600)
+
+def initPygame():
+	pygame.init()
+	pygame.midi.init()
+	# list all midi devices
+	#for x in range( 0, pygame.midi.get_count() ):
+	#	print pygame.midi.get_device_info(x)
+
+	# open a specific midi device
+	inp = pygame.midi.Input(3)	
+	# wait 10ms - this is arbitrary, but wait(0) still resulted
+	# in 100% cpu utilization
+	pygame.time.wait(10)	
+
+def startFreestyleMode():
+	midiThread = threading.Thread(target=freestyleListener, args=(inp,))
+	midiThread.daemon = True
+	midiThread.start()
+	freestyleBlenderState()
+
+def startPracticeMode():
+	initMidiScales(key, mode)
+	midiThread = threading.Thread(target=practiceListener, args=(inp,))
+	midiThread.daemon = True
+	midiThread.start()
+	practiceBlenderState()
+
+		
 
 
