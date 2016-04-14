3a53d44d43dbecec0a30f7b20820ec4768bd0c62
shadowfax6894 <anniekelly6894@gmail.com>
2016-04-13 21:27:19 -0600

    added more back buttons

--- scripts/blender.py
+++ scripts/blender.py
@@ -30,19 +30,25 @@ def practiceHoverOff(arg):
 	practiceButton.image = practiceButtonPhoto	
 
 def backToHomeScreen():
+	selectModeText.pack()
 	practiceButton.pack(side=RIGHT, padx=100)
-	freestyleButton.pack(side=LEFT, padx=50, pady=100)
+	freestyleButton.pack(side=LEFT, padx=50)
 	text.pack_forget()
 	backHomeButton.pack_forget()
 	keyOption.pack_forget()
 	modeOption.pack_forget()
 	okButton.pack_forget()
-	practiceText.pack_forget()
+	if practiceText:
+		practiceText.pack_forget()
 	optionsWidget.pack_forget()
 
 def practiceMode():
+	backToPracticeButton.pack_forget()
+	selectModeText.pack_forget()
 	freestyleButton.pack_forget()
-	practiceButton.pack_forget()	
+	practiceButton.pack_forget()
+	if practiceText:
+		practiceText.pack_forget()
 	optionsWidget.pack(pady=50)
 	keyOption.pack(side=LEFT)
 	modeOption.pack(side=RIGHT)
@@ -54,14 +60,15 @@ def practiceModeNext():
 	msg = "You've selected practice mode in the key of {0} {1}\nPlaying notes in key will turn the blender on!".format(selectedKey.get(), selectedMode.get())
 	practiceText = ttk.Label(mainframe, text=msg, fg="#FFFFFF", bg="#000000", pady=60, font=("courier",32))
 	keyOption.pack_forget()
+	backHomeButton.pack_forget()
 	modeOption.pack_forget()
 	okButton.pack_forget()
-	backHomeButton.pack_forget()
 	optionsWidget.pack_forget()
 	practiceText.pack()
-	backHomeButton.pack()
+	backToPracticeButton.pack()
 
 def freestyleMode():
+	selectModeText.pack_forget()
 	freestyleButton.pack_forget()
 	practiceButton.pack_forget()
 	text.pack()
@@ -76,29 +83,32 @@ mainframe = ttk.Frame(root)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
-mainframe.configure(background="#000000")
+mainframe.configure(background="#000000", highlightthickness=0)
 
 mainPhoto = ttk.PhotoImage(file="../images/rockblend.png")
 pic = ttk.Label(mainframe, image=mainPhoto, fg="#000000", bg="#000000")
 pic.pack(padx=(winWidth-mainImageWidth)/2)
+selectModeText = ttk.Label(mainframe, text="Select mode:", fg="#FFFFFF", bg="#000000", pady=40, font=("courier",20))
+selectModeText.pack()
 
 # Buttons #
 freestyleButtonPhoto = ttk.PhotoImage(file="../images/freestylehoverblack.png")
 freestyleButtonPhotoHover = ttk.PhotoImage(file="../images/freestyleblack.png")
-freestyleButton = ttk.Button(mainframe, image=freestyleButtonPhoto, fg="#000000", bg="#000000", command=freestyleMode)
+freestyleButton = ttk.Button(mainframe, image=freestyleButtonPhoto, fg="#000000", bg="#000000", borderwidth=0, highlightthickness=0, command=freestyleMode)
 freestyleButton.bind('<Enter>', freestyleHoverOn)
 freestyleButton.bind('<Leave>', freestyleHoverOff)
-freestyleButton.pack(side=LEFT, padx=50, pady=100)
+freestyleButton.pack(side=LEFT, padx=50)
 
 practiceButtonPhoto = ttk.PhotoImage(file="../images/practicehoverblack.png")
 practiceButtonPhotoHover = ttk.PhotoImage(file="../images/practiceblack.png")
-practiceButton = ttk.Button(mainframe, image=practiceButtonPhoto, fg="#000000", bg="#000000", command=practiceMode)
+practiceButton = ttk.Button(mainframe, image=practiceButtonPhoto, fg="#000000", bg="#000000", borderwidth=0, highlightthickness=0, command=practiceMode)
 practiceButton.bind('<Enter>', practiceHoverOn)
 practiceButton.bind('<Leave>', practiceHoverOff)
 practiceButton.pack(side=RIGHT, padx=100)
 
 #Freestyle page stuff
 backHomeButton = ttk.Button(mainframe, text="Go back", fg="#FFFFFF", bg="#000000", command=backToHomeScreen)
+backToPracticeButton = ttk.Button(mainframe, text="Go back", fg="#FFFFFF", bg="#000000", command=practiceMode)
 text = ttk.Label(mainframe, text="Freestyle mode\nTime to melt some faces!", fg="#FFFFFF", bg="#000000", pady=60, font=("courier",32))
 
 #Practice page stuff
